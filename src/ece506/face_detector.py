6ea868dc686e7fdfa05c70aa9a14891078814bc9
julopera <blitzrengar@gmail.com>
2025-12-09 03:18:15 -0500

    Fixed facial detection model loading.

--- src/ece506/face_detector.py
+++ src/ece506/face_detector.py
@@ -1,6 +1,13 @@
 import cv2
+from cv2 import FaceDetectorYN
+from pathlib import Path
 
-cv2.FaceDetectorYN_create('./face_detection_yunet_2023mar.onnx',
+model = Path('./src/models/face_detection_yunet_2023mar.onnx')
+
+cv2.FaceDetectorYN.create(str(model),
                           "", 
                           (300, 300),
                           score_threshold=0.5)
+
+
+print("Face detector model loaded from:", model)
